7d6500d3bb5d0eff283a10adeb89bfa2c015d714
Maksim <petrovmp89@gmail.com>
2021-05-10 12:26:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 async function run() {
   try {
     core.exportVariable('BRANCH_NAME', process.env.GITHUB_REF.split('/').slice(2).join('/'));
+    core.exportVariable('GITHUB_ACTOR', process.env.GITHUB_ACTOR);
   }
   catch (error) {
     core.setFailed(error.message);
