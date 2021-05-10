6fbeef206fac11e919c529602c1df9f772304038
Maksim Petrov <petrovmp89@gmail.com>
2021-05-10 13:20:29 +0300

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -120,6 +120,7 @@ const core = __webpack_require__(470);
 async function run() {
   try {
     core.exportVariable('BRANCH_NAME', process.env.GITHUB_REF.split('/').slice(2).join('/'));
+    core.exportVariable('GITHUB_ACTOR', process.env.GITHUB_ACTOR);
   }
   catch (error) {
     core.setFailed(error.message);
