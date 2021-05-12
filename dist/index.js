73b7497854b1115ea4d5f1d9a2fcd33846f9ecb4
Maksim Petrov <petrovmp89@gmail.com>
2021-05-12 12:17:24 +0300

    Update variables for export

--- dist/index.js
+++ dist/index.js
@@ -121,6 +121,7 @@ async function run() {
   try {
     core.exportVariable('BRANCH_NAME', process.env.GITHUB_REF.split('/').slice(2).join('/'));
     core.exportVariable('GITHUB_ACTOR', process.env.GITHUB_ACTOR);
+    core.exportVariable('GITHUB_SHA', process.env.GITHUB_SHA);
   }
   catch (error) {
     core.setFailed(error.message);
